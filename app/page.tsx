33c30606917f63e41d2adc54eeb14c817d3828a0
Jarren San Jose <sanjose.jarren@gmail.com>
2024-03-30 19:42:05 -0700

    change site metadata

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center">
       <main className="flex flex-col items-center p-24 flex-grow">
-        <h1 className="text-4xl font-bold mb-2">hi</h1>
+        <h1 className="text-4xl font-bold mb-2">denumb</h1>
         <p className="text-lg text-center mb-6">
           easily compare different denominations of cryptocurrency
         </p>
