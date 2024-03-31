219b8b095f015464449403d562f96a10b17ee48f
Jarren San Jose <sanjose.jarren@gmail.com>
2024-03-30 17:06:21 -0700

    show USD, too

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -14,6 +14,7 @@ const Calculator = () => {
   const [btc, setBtc] = useState('');
   const [eth, setEth] = useState('');
   const [sol, setSol] = useState('');
+  const [usd, setUsd] = useState('');
 
   useEffect(() => {
     const fetchPrices = async () => {
@@ -36,6 +37,7 @@ const Calculator = () => {
     if (!isNaN(numericValue)) {
       setEth(((numericValue * prices.bitcoin.usd) / prices.ethereum.usd).toString());
       setSol(((numericValue * prices.bitcoin.usd) / prices.solana.usd).toString());
+      setUsd((numericValue * prices.bitcoin.usd).toString());
     }
   };
 
@@ -46,6 +48,7 @@ const Calculator = () => {
     if (!isNaN(numericValue)) {
       setBtc(((numericValue * prices.ethereum.usd) / prices.bitcoin.usd).toString());
       setSol(((numericValue * prices.ethereum.usd) / prices.solana.usd).toString());
+      setUsd((numericValue * prices.ethereum.usd).toString());
     }
   };
 
@@ -56,8 +59,20 @@ const Calculator = () => {
     if (!isNaN(numericValue)) {
       setBtc(((numericValue * prices.solana.usd) / prices.bitcoin.usd).toString());
       setEth(((numericValue * prices.solana.usd) / prices.ethereum.usd).toString());
+      setUsd((numericValue * prices.solana.usd).toString());
     }
   };
+
+  const handleUsdChange = (value: string) => {
+    setUsd(value);
+    if (!prices) return;
+    const numericValue = parseFloat(value);
+    if (!isNaN(numericValue)) {
+      setBtc((numericValue / prices.bitcoin.usd).toString());
+      setEth((numericValue / prices.ethereum.usd).toString());
+      setSol((numericValue / prices.solana.usd).toString());
+    }
+  }
 
   return (
     <div className="flex flex-col space-y-4">
@@ -90,6 +105,16 @@ const Calculator = () => {
           onChange={(e) => handleSolChange(e.target.value)}
         />
         <span> SOL {prices ? `@ $${prices.solana.usd.toLocaleString()}` : ''}</span>
+      </div>
+      <div>
+        <input
+          type="text"
+          className="border p-2"
+          value={usd}
+          placeholder="usd"
+          onChange={(e) => handleUsdChange(e.target.value)}
+        />
+        <span> USD</span>
       </div>
       <CoinGeckoAttribution />
     </div>
